32e1daf7efb45cec7d25003be1916b6707bf9a82
laboprod <labeau.francois@gmail.com>
2021-02-09 17:15:30 +0100

    Message & Loader Components

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -14,7 +14,7 @@ export const listProducts = () => async (dispatch) => {
 		dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
 	} catch (error) {
 		dispatch({
-			PRODUCT_LIST_FAIL,
+			type: PRODUCT_LIST_FAIL,
 			payload:
 				error.response && error.response.data.message
 					? error.response.data.message
